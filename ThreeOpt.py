a8790a6811bd17bd27b5cad1a52181d18ffdcc3c
Emanuele Concas <conema@outlook.com>
2021-04-06 18:33:57 +0200

    3-opt using deposits

--- ThreeOpt.py
+++ ThreeOpt.py
@@ -1,22 +1,14 @@
 from utils import calculate_route_cost
 
 class ThreeOpt:
-    def __init__(self, cost_function, n):
+    def __init__(self, cost_function):
         """
         A simple 2-Opt class
 
             Parameters:
                 cost_function: a function which calculate the cost between two nodes
-                n: number of nodes in a route
         """
         self.cost_function = cost_function
-        self.all_segments_list = list(self.all_segments(n))
-
-    def all_segments(self, n):
-        return ((i, j, k)
-            for i in range(n)
-            for j in range(i + 2, n)
-            for k in range(j + 2, n + (i > 0)))
 
     def reverse_segment_if_better(self, tour, i, j, k):
         costs = []
@@ -31,11 +23,10 @@ class ThreeOpt:
         tours[1][j:k] = reversed(tour[j:k])
         tours[2][i:k] = reversed(tour[i:k])
 
-        tmp = tour[j:k] + tour[i:j]
-        tours[3][i:k] = tmp
+        tours[3][i:k] = tour[j:k] + tour[i:j]
 
         for tour in tours:
-            costs.append(calculate_route_cost(self.cost_function, tour))
+            costs.append(calculate_route_cost(self.cost_function, tour[1:-1]))
 
         min_index = costs.index(min(costs))
 
@@ -44,16 +35,23 @@ class ThreeOpt:
     def start(self, route, A, tabu_list):
         swaps = []
 
-        for (a, b, c) in filter(lambda x: not(
-                                                (route[x[0]-1], route[x[0]]) not in A 
-                                                or (route[x[1]-1], route[x[1]]) not in A
-                                                or (route[x[2]-1], route[x[2] % len(route)]) not in A 
-                                                or (route[x[0]], route[x[1]], route[x[2] % len(route)]
-                                            ) in tabu_list), self.all_segments_list):
-  
-            new_route, cost = self.reverse_segment_if_better(route, a, b, c)
+        route = [0]+route+[0]
 
-            swaps.append({"move": (route[a], route[b], route[c% len(route)]), "route": new_route, "cost": cost})
+        n = len(route)
+        
+        for i in range(1, n):
+            if (route[i-1], route[i]) not in A:
+                continue
+            for j in range(i + 1, n):
+                if (route[j-1], route[j]) not in A:
+                    continue
+                for k in range(j + 1, n):
+                    if (route[k-1], route[k]) not in A or (route[i], route[j], route[k]) in tabu_list:
+                        continue
+  
+                    new_route, cost = self.reverse_segment_if_better(route, i, j, k)
+
+                    swaps.append({"move": (route[i], route[j], route[k]), "route": new_route[1:-1], "cost": cost})
 
         # order routes using the cost
         return sorted(swaps, key=lambda k: k['cost']) 
